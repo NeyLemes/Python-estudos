e61defe14204e755b9ab2ee22dd9b2029b25b986
NeyLemes <131485382+NeyLemes@users.noreply.github.com>
2024-01-19 07:47:36 -0300

    Add files via upload

---
+++ string fatiamento.py
@@ -0,0 +1,9 @@
+nome = "claudinei Luiz Lemes da Silva"
+print(nome[0])
+print(nome[-1])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:15])
+print(nome[:])
+print(nome[::-1])
+print(nome[0:28:2])
---
+++ string multipla.py
@@ -0,0 +1,33 @@
+nome = "Claudinei"
+mensagem = f"""
+Olá meu nome é {nome},
+Estou aprendendo python
+"""
+
+print(mensagem)
+
+name = "Claudinei"
+messenger = f'''
+
+  Olá meu nome é {name},
+Estou aprendendo python
+    Esta mensagem tem diferentes recuos.
+'''
+print(messenger)
+
+print(
+"""
+===========MENU===========
+
+
+1 - DEPOSITAR
+
+2 - SACAR
+
+3 - SAIR
+
+==========================
+
+OBRIGADO POR UTILIZAR NOSSO SISTEMA!!
+"""
+)
